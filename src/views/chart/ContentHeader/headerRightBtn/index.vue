323fbbb52db9970a4ecc717d78560618286c8d9c
qiyubin <2878095417@qq.com>
2023-12-12 15:25:55 +0800

    首次提交

---
+++ src/views/chart/ContentHeader/headerRightBtn/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <n-space class="go-mt-0">
+    <n-button v-for="item in comBtnList" :key="item.title" :type="item.type" ghost @click="item.event">
+      <template #icon>
+        <component :is="item.icon"></component>
+      </template>
+      <span>{{ item.title }}</span>
+    </n-button>
+  </n-space>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { renderIcon, goDialog, fetchPathByName, routerTurnByPath, setSessionStorage, getLocalStorage } from '@/utils'
+import { PreviewEnum } from '@/enums/pageEnum'
+import { StorageEnum } from '@/enums/storageEnum'
+import { useRoute } from 'vue-router'
+import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore'
+import { syncData } from '../../ContentEdit/components/EditTools/hooks/useSyncUpdate.hook'
+import { icon } from '@/plugins'
+import { cloneDeep } from 'lodash'
+import { put } from '@/api/http'
+const { BrowsersOutlineIcon, SendIcon, AnalyticsIcon } = icon.ionicons5
+const chartEditStore = useChartEditStore()
+
+const routerParamsInfo = useRoute()
+
+// 预览
+const previewHandle = () => {
+  const path = fetchPathByName(PreviewEnum.CHART_PREVIEW_NAME, 'href')
+  if (!path) return
+  const { id } = routerParamsInfo.params
+  // id 标识
+  const previewId = typeof id === 'string' ? id : id[0]
+  const storageInfo = chartEditStore.getStorageInfo()
+  const sessionStorageInfo = getLocalStorage(StorageEnum.GO_CHART_STORAGE_LIST) || []
+
+  if (sessionStorageInfo?.length) {
+    const repeateIndex = sessionStorageInfo.findIndex((e: { id: string }) => e.id === previewId)
+    // 重复替换
+    if (repeateIndex !== -1) {
+      sessionStorageInfo.splice(repeateIndex, 1, { id: previewId, ...storageInfo })
+      setSessionStorage(StorageEnum.GO_CHART_STORAGE_LIST, sessionStorageInfo)
+    } else {
+      sessionStorageInfo.push({
+        id: previewId,
+        ...storageInfo
+      })
+      setSessionStorage(StorageEnum.GO_CHART_STORAGE_LIST, sessionStorageInfo)
+    }
+  } else {
+    setSessionStorage(StorageEnum.GO_CHART_STORAGE_LIST, [{ id: previewId, ...storageInfo }])
+  }
+  // 跳转
+  routerTurnByPath(path, [previewId], undefined, true)
+}
+
+// 发布
+const sendHandle = () => {
+  const { id } = routerParamsInfo.params
+  // console.log(chartEditStore.getStorageInfo())
+  // 保存按钮在这里！！
+  put('/data-service/api/updateDataSource', {
+    largeScreenID: id[0],
+    dataSource: JSON.stringify(chartEditStore.getStorageInfo())
+  }).then(res => {
+    window['$message'].success('保存成功！')
+  })
+}
+
+const btnList = [
+  {
+    select: true,
+    title: '同步内容',
+    type: 'primary',
+    icon: renderIcon(AnalyticsIcon),
+    event: syncData
+  },
+  {
+    select: true,
+    title: '预览',
+    icon: renderIcon(BrowsersOutlineIcon),
+    event: previewHandle
+  },
+  {
+    select: true,
+    title: '保存',
+    icon: renderIcon(SendIcon),
+    event: sendHandle
+  }
+]
+
+const comBtnList = computed(() => {
+  if (chartEditStore.getEditCanvas.isCodeEdit) {
+    return btnList
+  }
+  const cloneList = cloneDeep(btnList)
+  cloneList.shift()
+  return cloneList
+})
+</script>
+
+<style lang="scss" scoped>
+.align-center {
+  margin-top: -4px;
+}
+</style>
---
+++ src/views/preview/utils/storage.ts
@@ -0,0 +1,45 @@
+import { getSessionStorage } from '@/utils'
+import { StorageEnum } from '@/enums/storageEnum'
+import { ChartEditStorage } from '@/store/modules/chartEditStore/chartEditStore.d'
+import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore'
+import { get } from '@/api/http'
+
+const chartEditStore = useChartEditStore()
+
+export interface ChartEditStorageType extends ChartEditStorage {
+  id: string
+}
+
+// 根据路由 id 获取存储数据的信息
+export const getSessionStorageInfo = () => {
+  const urlHash = document.location.hash
+  const toPathArray = urlHash.split('/')
+  const id = toPathArray && toPathArray[toPathArray.length - 1]
+  // const storageList: ChartEditStorageType[] = getSessionStorage(
+  //   StorageEnum.GO_CHART_STORAGE_LIST
+  // )
+
+  get('data-service/api/byId', {
+    largeScreenID: id.toString()
+  }).then(res => {
+    if (res.code == 0) {
+      let dataSource = JSON.parse(res.data.source[0].data_source)
+      const { editCanvasConfig, requestGlobalConfig, componentList } = dataSource
+          chartEditStore.editCanvasConfig = editCanvasConfig
+          chartEditStore.requestGlobalConfig = requestGlobalConfig
+          chartEditStore.componentList = componentList
+      return dataSource
+    }
+  })
+  // console.log('storageList', id.toString())
+  // for (let i = 0; i < storageList.length; i++) {
+  //   if (id.toString() === storageList[i]['large_screen_iD']) {
+  //     const { editCanvasConfig, requestGlobalConfig, componentList } = storageList[i]
+  //     chartEditStore.editCanvasConfig = editCanvasConfig
+  //     chartEditStore.requestGlobalConfig = requestGlobalConfig
+  //     chartEditStore.componentList = componentList
+  //     return storageList[i]
+  //   }
+  // }
+
+}
